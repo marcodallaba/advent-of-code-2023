dd2964b02077dd22204dcfda6a15d0039b1917ff
Marco Dalla Ba <marco.dallaba@vimar.com>
2023-12-02 10:12:02 +0100

    First Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,16 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+
+    var sum = 0L
+    for (s in input) {
+        val firstDigit = s.first {
+            it.isDigit()
+        }.digitToInt()
+        val lastDigit = s.last {
+            it.isDigit()
+        }.digitToInt()
+        sum += (firstDigit * 10 + lastDigit)
+    }
+    sum.println()
 }
