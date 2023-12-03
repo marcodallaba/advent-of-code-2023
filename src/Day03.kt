5c4916f490a242883a1ed9895744107d717a9b3a
Marco Dalla Ba <marco.dallaba@vimar.com>
2023-12-03 18:03:44 +0100

    Day03 second half solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,4 @@
+import java.util.HashMap
 import java.util.Scanner
 import kotlin.math.max
 
@@ -5,7 +6,7 @@ fun main() {
 
     val input = readInput("Day03")
 
-    firstHalf03(input)
+    secondHalf03(input)
 }
 
 fun firstHalf03(input: List<String>) {
@@ -97,6 +98,139 @@ fun firstHalf03(input: List<String>) {
                 }
 
             }
+        }
+    }
+    println(sum)
+}
+
+fun secondHalf03(input: List<String>) {
+    val map = HashMap<Pair<Int, Int>, MutableList<Int>>()
+
+    input.forEachIndexed { rowIndex, s ->
+
+        var currentNum = 0
+        s.forEachIndexed { rightIndex, c ->
+
+            if (c.isDigit()) {
+                currentNum = currentNum * 10 + c.digitToInt()
+                if (rightIndex == s.length - 1) {
+
+                    val length = currentNum.toString().length
+                    val leftIndex = rightIndex - length
+
+                    if (leftIndex >= 0 && s[leftIndex] == '*') {
+                        val pair = Pair(rowIndex, leftIndex)
+                        val list = map.getOrDefault(pair, mutableListOf())
+                        list.add(currentNum)
+                        map[pair] = list
+                    }
+
+                    val limitedLeftIndex = if (leftIndex >= 0) {
+                        leftIndex
+                    } else {
+                        0
+                    }
+
+                    val limitedRightIndex = if (rightIndex + 1 < s.length) {
+                        rightIndex + 1
+                    } else {
+                        rightIndex
+                    }
+
+                    if (rowIndex > 0) {
+                        input[rowIndex - 1].substring(limitedLeftIndex, limitedRightIndex).forEachIndexed { index, c ->
+                            if (c == '*') {
+                                val pair = Pair(rowIndex - 1, limitedLeftIndex + index)
+
+                                val list = map.getOrDefault(pair, mutableListOf())
+                                list.add(currentNum)
+                                map[pair] = list
+                            }
+                        }
+                    }
+
+                    if (rowIndex < input.size - 1) {
+                        input[rowIndex + 1].substring(limitedLeftIndex, limitedRightIndex).forEachIndexed { index, c ->
+                            if (c == '*') {
+                                val pair = Pair(rowIndex + 1, limitedLeftIndex + index)
+
+                                val list = map.getOrDefault(pair, mutableListOf())
+                                list.add(currentNum)
+                                map[pair] = list
+                            }
+                        }
+                    }
+
+                    currentNum = 0
+                }
+            } else {
+                if (currentNum > 0) {
+                    val length = currentNum.toString().length
+                    val leftIndex = rightIndex - length - 1
+
+                    if (c == '*') {
+                        val pair = Pair(rowIndex, rightIndex)
+
+                        val list = map.getOrDefault(pair, mutableListOf())
+                        list.add(currentNum)
+                        map[pair] = list
+                    }
+
+                    if (leftIndex >= 0 && s[leftIndex] == '*') {
+                        val pair = Pair(rowIndex, leftIndex)
+
+                        val list = map.getOrDefault(pair, mutableListOf())
+                        list.add(currentNum)
+                        map[pair] = list
+                    }
+
+                    val limitedLeftIndex = if (leftIndex >= 0) {
+                        leftIndex
+                    } else {
+                        0
+                    }
+
+                    val limitedRightIndex = if (rightIndex + 1 < s.length) {
+                        rightIndex + 1
+                    } else {
+                        rightIndex
+                    }
+
+                    if (rowIndex > 0) {
+                        input[rowIndex - 1].substring(limitedLeftIndex, limitedRightIndex).forEachIndexed { index, c ->
+                            if (c == '*') {
+                                val pair = Pair(rowIndex - 1, limitedLeftIndex + index)
+
+                                val list = map.getOrDefault(pair, mutableListOf())
+                                list.add(currentNum)
+                                map[pair] = list
+                            }
+                        }
+                    }
+
+                    if (rowIndex < input.size - 1) {
+                        input[rowIndex + 1].substring(limitedLeftIndex, limitedRightIndex).forEachIndexed { index, c ->
+                            if (c == '*') {
+
+                                val pair = Pair(rowIndex + 1, limitedLeftIndex + index)
+
+                                val list = map.getOrDefault(pair, mutableListOf())
+                                list.add(currentNum)
+                                map[pair] = list
+                            }
+                        }
+                    }
+
+                    currentNum = 0
+                }
+
+            }
+        }
+    }
+    var sum = 0
+    map.forEach {
+        if (it.value.size == 2) {
+            sum += (it.value[0] * it.value[1])
         }
     }
     println(sum)
