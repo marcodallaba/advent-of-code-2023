291269215ddedbf3e12371007ab063fd99e4c7d8
Marco Dalla Ba <marco.dallaba@vimar.com>
2023-12-03 12:10:05 +0100

    Day02 second half solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,10 +1,12 @@
 import java.util.Scanner
+import kotlin.math.max
 
 fun main() {
 
     val input = readInput("Day02")
 
-    firstHalf02(input)
+    // firstHalf02(input)
+    secondHalf02(input)
 }
 
 fun firstHalf02(input: List<String>) {
@@ -56,4 +58,45 @@ fun firstHalf02(input: List<String>) {
     }
 
     idSum.println()
+}
+
+fun secondHalf02(input: List<String>) {
+
+    var powersSum = 0
+
+    input.forEachIndexed { index, game ->
+        val substring = game.substring(game.indexOf(':') + 1)
+
+        var maxRed = 0
+        var maxGreen = 0
+        var maxBlue = 0
+
+        val sets = substring.split(';')
+        sets.forEach { set ->
+            val cubes = set.split(',')
+
+            cubes.forEach {
+                val cube = it.trim()
+                val scanner = Scanner(cube)
+                val value = scanner.nextInt()
+                val color = scanner.next()
+                when (color) {
+                    "red" -> {
+                        maxRed = max(maxRed, value)
+                    }
+
+                    "green" -> {
+                        maxGreen = max(maxGreen, value)
+                    }
+
+                    "blue" -> {
+                        maxBlue = max(maxBlue, value)
+                    }
+                }
+            }
+        }
+        powersSum += (maxRed * maxGreen * maxBlue)
+    }
+
+    powersSum.println()
 }
